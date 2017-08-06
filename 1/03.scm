009b52130f17724307c0bcaaad0a5d42797b9de3
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2017-08-06 15:24:19 +0300

    Minor formatting in exercise 1.3

--- 1/03.scm
+++ 1/03.scm
@@ -1,7 +1,7 @@
 ; SICP exercise 1.3
 ;
-; Define a procedure that takes three numbers as arguments and returns the sum of
-; the squares of the two larger numbers.
+; Define a procedure that takes three numbers as arguments and returns the sum
+; of the squares of the two larger numbers.
 
 (define (square x) (* x x))
 
